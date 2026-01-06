bc1992a9129b0f4f54cf41e6f0832b3bb6266ce5
vincent.karlsson94@gmail.com <vincent.karlsson94@gmail.com>
2026-01-06 12:54:50 +0100

    Update migration scripts with asd

--- database/.migrations/003-add-like-button.sql
+++ database/.migrations/003-add-like-button.sql
@@ -1,7 +1,9 @@
 ALTER TABLE api.blog 
 ADD COLUMN IF NOT EXISTS likes bigint DEFAULT 0;
 
-ALTER TABLE api.blog
-ADD COLUMN IF NOT EXISTS author text;
+ALTER TABLE api.users
+ADD COLUMN IF NOT EXISTS fullname text;
 
-GRANT UPDATE (likes) ON api.blog TO anon
+GRANT UPDATE (likes) ON api.blog TO anon;
+GRANT SELECT (id, fullname) ON api.users TO anon;
+GRANT SELECT, UPDATE on api.users to authenticated;
